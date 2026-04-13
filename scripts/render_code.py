382030627f73082522623a5a73bf437e60aa06b3
soykan8-maarifmatematik <soykan8@gmail.com>
2026-04-13 18:54:15 +0300

    Update render_code.py

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -3,12 +3,13 @@ from manim import *
 
 class UnitFractionLogic(Scene):
     def construct(self):
-        # --- TASARIM STANDARTLARI ---
+        # --- TASARIM AYARLARI ---
         self.camera.background_color = "#FFFFFF"
         dark_grey = "#333333"
         maarif_blue = "#87CEEB"
         
-        # 1. MERKEZ NOKTASI (Tüm objeler buraya kilitlenecek)
+        # 1. SABİT MERKEZ NOKTASI
+        # Tüm objeler ekranın tam ortasından biraz aşağıda (DOWN * 0.5) toplanacak
         main_center = DOWN * 0.5 
 
         # 2. Başlık
@@ -21,7 +22,7 @@ class UnitFractionLogic(Scene):
         whole_circle.move_to(main_center)
 
         # 4. Eşit Bölme Çizgileri
-        # Çizgilerin merkezini dairenin merkezine göre hesaplıyoruz
+        # Çizgilerin merkezini dairenin merkezine mühürlüyoruz
         h_line = Line(
             whole_circle.get_left(), whole_circle.get_right(), 
             color=dark_grey, stroke_width=2
@@ -32,10 +33,10 @@ class UnitFractionLogic(Scene):
         )
         lines = VGroup(h_line, v_line)
 
-        # 5. Birim Kesir Dilimi (KRİTİK DÜZELTME)
-        # arc_center=main_center diyerek dilimin "sivri ucunu" dairenin merkezine çiviliyoruz
+        # 5. Birim Kesir Dilimi (KRİTİK HİZALAMA)
+        # arc_center=main_center diyerek dilimin sivri ucunu dairenin merkezine çiviliyoruz
         unit_slice = Sector(
-            arc_center=main_center, # Tam merkezde olması için kilit nokta
+            arc_center=main_center, 
             inner_radius=0,
             outer_radius=2.2,
             angle=90 * DEGREES,
@@ -48,7 +49,7 @@ class UnitFractionLogic(Scene):
 
         # 6. Altyazı Kutusu ve Metni
         subtitle_box = Rectangle(
-            width=8.5, height=1, 
+            width=8.5, height=1.2, 
             fill_color=dark_grey, fill_opacity=0.8, 
             stroke_width=0
         ).to_edge(DOWN, buff=0.8)
@@ -59,22 +60,22 @@ class UnitFractionLogic(Scene):
         ).scale(0.45)
         caption.move_to(subtitle_box.get_center())
 
-        # --- ANİMASYON ---
+        # --- ANİMASYON AKIŞI ---
         self.play(Write(title))
         self.wait(0.3)
         
         self.play(Create(whole_circle), Create(lines))
         self.wait(0.5)
 
-        # Dilimin merkezden dışarı doğru zarifçe gelmesi
-        self.play(FadeIn(unit_slice, shift=RIGHT*0.2 + DOWN*0.2)) 
+        # Dilimin tam merkezden belirmesi
+        self.play(FadeIn(unit_slice)) 
         self.wait(0.2)
         
         # Altyazı
         self.play(FadeIn(subtitle_box, shift=UP), Write(caption))
         self.wait(4)
 
-        # Temizlik
+        # Kapanış
         self.play(
             FadeOut(VGroup(whole_circle, lines, unit_slice, title, subtitle_box, caption))
         )
