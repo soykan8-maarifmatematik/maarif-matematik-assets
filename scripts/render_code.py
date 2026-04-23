2e8f0abb4c5153054f8cd493c77de81bf76fe859
soykan8-maarifmatematik <soykan8@gmail.com>
2026-04-23 13:59:00 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -1,56 +1,49 @@
 from manim import *
-import numpy as np
 
 config.pixel_height = 1920
 config.pixel_width = 1080
 config.frame_height = 14.22
 config.frame_width = 8.0
 
-class MaarifScene(Scene):
+class BirimKesirler(Scene):
     def construct(self):
-        caption1 = Text("Paydasi buyuk olan", font_size=85).shift(UP * 4.5)
-        caption1.scale_to_fit_width(7)
-        caption2 = Text("her zaman buyuk mudur?", font_size=85).shift(DOWN * 4.5)
-        caption2.scale_to_fit_width(7)
-        self.play(Write(caption1), Write(caption2))
+        hook_text = Text("Pastayı 2'ye mi bölsen", font="sans-serif", font_size=80).scale_to_fit_width(7.2).to_edge(UP, buff=1.0)
+        hook_text2 = Text("daha büyük dilim yersin 8'e mi?", font="sans-serif", font_size=80).scale_to_fit_width(7.2).next_to(hook_text, DOWN, buff=0.5)
+        
+        self.play(Write(hook_text), Write(hook_text2))
+        self.wait(1)
+        self.play(FadeOut(hook_text), FadeOut(hook_text2))
+
+        circle_half = Circle(radius=1.5, color=WHITE)
+        frac_half = MathTex(r"\frac{1}{2}", font_size=120)
+        group_half = VGroup(circle_half, frac_half).arrange(DOWN, buff=0.5)
+
+        circle_eighth = Circle(radius=1.5, color=WHITE)
+        frac_eighth = MathTex(r"\frac{1}{8}", font_size=120)
+        group_eighth = VGroup(circle_eighth, frac_eighth).arrange(DOWN, buff=0.5)
+
+        comparison_group = VGroup(group_half, group_eighth).arrange(DOWN, buff=1.5).move_to(ORIGIN)
+
+        sector_half = Sector(radius=1.5, angle=TAU/2, arc_center=circle_half.get_center(), color=YELLOW, fill_opacity=0.8)
+        sector_eighth = Sector(radius=1.5, angle=TAU/8, arc_center=circle_eighth.get_center(), color=RED, fill_opacity=0.8)
+
+        self.play(FadeIn(circle_half), Write(frac_half))
+        self.play(Create(sector_half))
+        self.wait(1)
+
+        self.play(FadeIn(circle_eighth), Write(frac_eighth))
+        self.play(Create(sector_eighth))
+        self.wait(1)
+
+        rule_text = Text("Payda büyüdükçe dilim küçülür!", font="sans-serif", font_size=80).scale_to_fit_width(7.2).to_edge(DOWN, buff=2.0)
+        self.play(Write(rule_text))
+        self.play(Indicate(sector_half), Indicate(sector_eighth))
         self.wait(2)
-        self.play(FadeOut(caption1), FadeOut(caption2))
-        circle = Circle(radius=3, color=WHITE)
-        self.play(Create(circle))
-        sector2 = Sector(radius=3, angle=PI, color=RED, fill_opacity=0.8)
-        sector2.move_to(circle.get_center())
-        frac_half = MathTex(r"\frac{1}{2}", font_size=110).shift(UP * 4.5)
-        caption3 = Text("Bir pizzayi 2ye bol", font_size=85).shift(DOWN * 4.5)
-        caption3.scale_to_fit_width(7)
-        self.play(Create(sector2), Write(frac_half), Write(caption3))
-        self.wait(2)
-        sector4 = Sector(radius=3, angle=PI/2, color=BLUE, fill_opacity=0.8)
-        sector4.move_to(circle.get_center())
-        frac_quarter = MathTex(r"\frac{1}{4}", font_size=110).shift(UP * 4.5)
-        caption4 = Text("4e bolersen dilim kuculur", font_size=85).shift(DOWN * 4.5)
-        caption4.scale_to_fit_width(7)
-        self.play(ReplacementTransform(sector2, sector4), ReplacementTransform(frac_half, frac_quarter), ReplacementTransform(caption3, caption4))
-        self.wait(2)
-        sector8 = Sector(radius=3, angle=PI/4, color=GREEN, fill_opacity=0.8)
-        sector8.move_to(circle.get_center())
-        frac_eighth = MathTex(r"\frac{1}{8}", font_size=110).shift(UP * 4.5)
-        caption5 = Text("8e bolersen daha da kuculur", font_size=85).shift(DOWN * 4.5)
-        caption5.scale_to_fit_width(7)
-        self.play(ReplacementTransform(sector4, sector8), ReplacementTransform(frac_quarter, frac_eighth), ReplacementTransform(caption4, caption5))
-        self.wait(2)
-        caption6 = Text("Yani payda buyudukce", font_size=85).shift(DOWN * 3.5)
-        caption6.scale_to_fit_width(7)
-        caption7 = Text("kesir kuculur", font_size=85).shift(DOWN * 4.5)
-        caption7.scale_to_fit_width(7)
-        self.play(ReplacementTransform(caption5, caption6), Write(caption7))
-        self.play(Indicate(frac_eighth))
-        self.wait(2)
-        self.play(FadeOut(circle), FadeOut(sector8), FadeOut(frac_eighth), FadeOut(caption6), FadeOut(caption7))
-        cta1 = Text("Maarif Matematik ile", font_size=85).shift(UP * 1)
-        cta1.scale_to_fit_width(7)
-        cta2 = Text("mantigini kavra", font_size=85).shift(DOWN * 0.5)
-        cta2.scale_to_fit_width(7)
-        cta3 = Text("takipte kal", font_size=85).shift(DOWN * 2)
-        cta3.scale_to_fit_width(7)
-        self.play(Write(cta1), Write(cta2), Write(cta3))
-        self.wait(2)
+
+        self.play(FadeOut(comparison_group), FadeOut(sector_half), FadeOut(sector_eighth), FadeOut(rule_text))
+
+        cta_text = Text("Maarif Matematik ile mantığını kavra", font="sans-serif", font_size=80).scale_to_fit_width(7.2).to_edge(UP, buff=5.0)
+        cta_text2 = Text("takipte kal!", font="sans-serif", font_size=80).scale_to_fit_width(7.2).next_to(cta_text, DOWN, buff=0.5)
+        self.play(Write(cta_text), Write(cta_text2))
+        self.play(Wiggle(cta_text2))
+        self.wait(1)
