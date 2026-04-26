81d5538334100c465b25dce80a931b83250eac74
soykan8-maarifmatematik <soykan8@gmail.com>
2026-04-27 00:12:29 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -2,92 +2,93 @@ from manim import *
 
 class MaarifScene(Scene):
     def construct(self):
-        self.camera.background_color = "#FFFFFF"
+        self.camera.background_color = '#FFFFFF'
         
-        # BÖLÜM 1: Basit Kesir (Süre: 24.1s)
-        title1 = Text("Basit Kesir: 3/4", color=BLACK, font_size=40).to_edge(UP)
+        # Part 1: Basit Kesir
+        intro_text = Text('Basit Kesirler', color=BLACK, font_size=48).to_edge(UP)
+        self.play(Write(intro_text), run_time=2.0)
         
-        circle1 = Circle(radius=1.5, color=BLACK).move_to(DOWN * 0.5)
-        sector1_1 = Sector(radius=1.5, angle=PI/2, start_angle=0, color=BLUE, fill_opacity=0.7).move_to(DOWN * 0.5)
-        sector1_2 = Sector(radius=1.5, angle=PI/2, start_angle=PI/2, color=BLUE, fill_opacity=0.7).move_to(DOWN * 0.5)
-        sector1_3 = Sector(radius=1.5, angle=PI/2, start_angle=PI, color=BLUE, fill_opacity=0.7).move_to(DOWN * 0.5)
+        c_outline = Circle(radius=1.5, color=BLACK).move_to(LEFT * 2)
+        self.play(Create(c_outline), run_time=1.0)
         
-        lines1 = VGroup(
-            Line(DOWN * 0.5, DOWN * 0.5 + RIGHT * 1.5, color=BLACK),
-            Line(DOWN * 0.5, DOWN * 0.5 + UP * 1.5, color=BLACK),
-            Line(DOWN * 0.5, DOWN * 0.5 + LEFT * 1.5, color=BLACK),
-            Line(DOWN * 0.5, DOWN * 0.5 + DOWN * 1.5, color=BLACK)
-        )
-
-        self.play(Write(title1), run_time=2.0)
-        self.play(Create(circle1), Create(lines1), run_time=2.0)
-        self.play(Create(sector1_1), Create(sector1_2), Create(sector1_3), run_time=3.0)
-        self.wait(17.1)
-
-        # BÖLÜM 2: Bileşik Kesir (Süre: 27.6s)
-        self.play(FadeOut(*self.mobjects), run_time=1.0)
+        l1 = Line(c_outline.get_top(), c_outline.get_bottom(), color=BLACK)
+        l2 = Line(c_outline.get_left(), c_outline.get_right(), color=BLACK)
+        self.play(Create(VGroup(l1, l2)), run_time=1.0)
         
-        title2 = Text("Bileşik Kesir: 5/4", color=BLACK, font_size=40).to_edge(UP)
+        s1 = Sector(radius=1.5, angle=PI/2, start_angle=0, color=BLUE, fill_opacity=0.7).move_to(LEFT * 2)
+        s2 = Sector(radius=1.5, angle=PI/2, start_angle=PI/2, color=BLUE, fill_opacity=0.7).move_to(LEFT * 2)
+        s3 = Sector(radius=1.5, angle=PI/2, start_angle=PI, color=BLUE, fill_opacity=0.7).move_to(LEFT * 2)
         
-        circle2_1 = Circle(radius=1.5, color=BLACK).move_to(LEFT * 2 + DOWN * 0.5)
-        circle2_2 = Circle(radius=1.5, color=BLACK).move_to(RIGHT * 2 + DOWN * 0.5)
+        self.play(Create(VGroup(s1, s2, s3)), run_time=2.0)
         
-        lines2_1 = VGroup(
-            Line(circle2_1.get_center(), circle2_1.get_center() + RIGHT * 1.5, color=BLACK),
-            Line(circle2_1.get_center(), circle2_1.get_center() + UP * 1.5, color=BLACK),
-            Line(circle2_1.get_center(), circle2_1.get_center() + LEFT * 1.5, color=BLACK),
-            Line(circle2_1.get_center(), circle2_1.get_center() + DOWN * 1.5, color=BLACK)
-        )
+        frac1 = MathTex(r'\frac{3}{4}', color=BLACK, font_size=72).next_to(c_outline, RIGHT, buff=1.0)
+        self.play(Write(frac1), run_time=2.0)
         
-        lines2_2 = VGroup(
-            Line(circle2_2.get_center(), circle2_2.get_center() + RIGHT * 1.5, color=BLACK),
-            Line(circle2_2.get_center(), circle2_2.get_center() + UP * 1.5, color=BLACK),
-            Line(circle2_2.get_center(), circle2_2.get_center() + LEFT * 1.5, color=BLACK),
-            Line(circle2_2.get_center(), circle2_2.get_center() + DOWN * 1.5, color=BLACK)
-        )
-
-        full_sector = Sector(radius=1.5, angle=TAU, start_angle=0, color=RED, fill_opacity=0.7).move_to(circle2_1.get_center())
-        extra_sector = Sector(radius=1.5, angle=PI/2, start_angle=0, color=RED, fill_opacity=0.7).move_to(circle2_2.get_center())
-
-        self.play(Write(title2), run_time=2.0)
-        self.play(Create(circle2_1), Create(lines2_1), Create(circle2_2), Create(lines2_2), run_time=2.0)
-        self.play(Create(full_sector), run_time=2.0)
-        self.play(Create(extra_sector), run_time=2.0)
-        self.wait(18.6)
-
-        # BÖLÜM 3: Tam Sayılı Kesir ve Bölme Evi (Süre: 27.1s)
-        self.play(FadeOut(*self.mobjects), run_time=1.0)
+        self.wait(20.23)
         
-        title3 = Text("Tam Sayılı Kesire Çevirme", color=BLACK, font_size=40).to_edge(UP)
+        part1_group = VGroup(intro_text, c_outline, l1, l2, s1, s2, s3, frac1)
         
-        dividend = MathTex("5", color=BLACK, font_size=60).move_to(LEFT * 0.8 + UP * 0.5)
-        divisor = MathTex("4", color=BLACK, font_size=60).move_to(RIGHT * 0.8 + UP * 0.5)
+        # Part 2: Bilesik Kesir
+        self.play(FadeOut(part1_group), run_time=1.0)
         
-        vert_line = Line(UP * 1.2, DOWN * 0.2, color=BLACK).move_to(ORIGIN + UP * 0.5)
-        horiz_line = Line(RIGHT * 0.2, RIGHT * 1.4, color=BLACK).next_to(divisor, DOWN, buff=0.1)
+        c1 = Circle(radius=1.2, color=BLACK).move_to(LEFT * 3)
+        c2 = Circle(radius=1.2, color=BLACK).move_to(ORIGIN)
+        self.play(Create(VGroup(c1, c2)), run_time=2.0)
         
-        quotient = MathTex("1", color=BLACK, font_size=60).next_to(horiz_line, DOWN, buff=0.2)
+        sectors = VGroup()
+        for i in range(4):
+            sectors.add(Sector(radius=1.2, angle=PI/2, start_angle=i*PI/2, color=RED, fill_opacity=0.7).move_to(LEFT * 3))
+        for i in range(3):
+            sectors.add(Sector(radius=1.2, angle=PI/2, start_angle=i*PI/2, color=RED, fill_opacity=0.7).move_to(ORIGIN))
+            
+        self.play(Create(sectors), run_time=2.0)
         
-        sub_val = MathTex("4", color=BLACK, font_size=60).next_to(dividend, DOWN, buff=0.5)
-        minus_sign = MathTex("-", color=BLACK, font_size=60).next_to(sub_val, LEFT, buff=0.1)
-        sub_line = Line(LEFT * 1.4, LEFT * 0.2, color=BLACK).next_to(sub_val, DOWN, buff=0.1)
+        frac2 = MathTex(r'\frac{7}{4}', color=BLACK, font_size=72).next_to(c2, RIGHT, buff=1.0)
+        self.play(Write(frac2), run_time=2.0)
         
-        remainder = MathTex("1", color=BLACK, font_size=60).next_to(sub_line, DOWN, buff=0.2)
+        self.wait(19.47)
         
-        eq_text = MathTex(r"\frac{5}{4} = 1 \frac{1}{4}", color=BLACK, font_size=60).move_to(DOWN * 2)
-
-        self.play(Write(title3), run_time=2.0)
-        self.play(Write(dividend), Write(divisor), run_time=2.0)
-        self.play(Create(vert_line), Create(horiz_line), run_time=2.0)
-        self.play(Write(quotient), run_time=2.0)
-        self.play(Write(sub_val), Write(minus_sign), run_time=2.0)
-        self.play(Create(sub_line), Write(remainder), run_time=2.0)
-        self.play(Write(eq_text), run_time=3.0)
-        self.wait(11.1)
-
-        # BÖLÜM 4: Kapanış (Süre: 10.0s)
-        self.play(FadeOut(*self.mobjects), run_time=1.0)
-        outro_text = Text("Maarif Matematik", color=BLACK, font_size=50)
+        part2_group = VGroup(c1, c2, sectors, frac2)
+        
+        # Part 3: Tam Sayili Kesir
+        self.play(part2_group.animate.shift(LEFT * 2), run_time=2.0)
+        
+        frac3 = MathTex(r'1 \frac{3}{4}', color=BLACK, font_size=72).next_to(frac2, RIGHT, buff=1.0)
+        self.play(Write(frac3), run_time=2.0)
+        
+        self.wait(21.88)
+        
+        part3_group = VGroup(part2_group, frac3)
+        
+        # Part 4: Cevirme ve Bolme Evi
+        self.play(FadeOut(part3_group), run_time=1.0)
+        
+        dividend = MathTex('7', color=BLACK, font_size=72).move_to(LEFT * 0.5 + UP * 0.5)
+        divisor = MathTex('4', color=BLACK, font_size=72).move_to(RIGHT * 0.5 + UP * 0.5)
+        self.play(Write(VGroup(dividend, divisor)), run_time=2.0)
+        
+        v_line = Line(UP * 1.2, DOWN * 0.2, color=BLACK)
+        h_line = Line(ORIGIN, RIGHT * 1.2, color=BLACK)
+        self.play(Create(VGroup(v_line, h_line)), run_time=2.0)
+        
+        quotient = MathTex('1', color=BLACK, font_size=72).next_to(h_line, DOWN, buff=0.3)
+        self.play(Write(quotient), run_time=1.0)
+        
+        remainder = MathTex('3', color=BLACK, font_size=72).next_to(dividend, DOWN, buff=0.8)
+        self.play(Write(remainder), run_time=1.0)
+        
+        final_frac = MathTex(r'1 \frac{3}{4}', color=BLACK, font_size=72).move_to(RIGHT * 3 + DOWN * 1)
+        self.play(Write(final_frac), run_time=2.0)
+        
+        self.wait(16.29)
+        
+        part4_group = VGroup(dividend, divisor, v_line, h_line, quotient, remainder, final_frac)
+        
+        # Part 5: Kapanis
+        self.play(FadeOut(part4_group), run_time=1.0)
+        
+        outro_text = Text('Maarif Matematik', color=BLACK, font_size=60)
         self.play(Write(outro_text), run_time=2.0)
-        self.wait(6.0)
-        self.wait(1.0) # Hoşça kalın dedikten sonra tam 1 saniye bekleme
+        
+        self.wait(5.41)
+        self.wait(1.0)
