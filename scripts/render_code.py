58a32b3fa80a5fdea826fda714a979b666f2476e
soykan8-maarifmatematik <soykan8@gmail.com>
2026-04-30 00:03:54 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -1,5 +1,4 @@
 from manim import *
-
 config.pixel_height = 1920
 config.pixel_width = 1080
 
@@ -7,38 +6,39 @@ class MaarifScene(Scene):
     def construct(self):
         self.camera.background_color = "#FFFFFF"
         
-        title = Text("Birim Kesirlerin Büyüklüğü", color=BLACK, weight=BOLD).to_edge(UP, buff=1.5)
+        title = Text("BIRIM KESIRLER", color=BLACK, weight=BOLD).scale(1.1).to_edge(UP, buff=1.2)
         self.play(Write(title))
-        self.wait(5.0)
+        self.wait(1.7)
         
-        pizza1 = Circle(radius=2.5, color=BLACK).shift(UP * 1.5 + LEFT * 3)
-        pizza2 = Circle(radius=2.5, color=BLACK).shift(UP * 1.5 + RIGHT * 3)
-        self.play(Create(pizza1), Create(pizza2))
+        c1_outline = Circle(radius=1.5, color=BLACK, stroke_width=4)
+        c1_line = Line(c1_outline.get_top(), c1_outline.get_bottom(), color=BLACK)
+        c1_sector = Sector(radius=1.5, angle=PI, start_angle=PI/2, color=BLUE, fill_opacity=0.8)
+        model1 = VGroup(c1_outline, c1_line, c1_sector)
+        
+        c2_outline = Circle(radius=1.5, color=BLACK, stroke_width=4)
+        c2_line1 = Line(c2_outline.get_top(), c2_outline.get_bottom(), color=BLACK)
+        c2_line2 = Line(c2_outline.get_left(), c2_outline.get_right(), color=BLACK)
+        c2_sector = Sector(radius=1.5, angle=PI/2, start_angle=PI/2, color=RED, fill_opacity=0.8)
+        model2 = VGroup(c2_outline, c2_line1, c2_line2, c2_sector)
+        
+        models = VGroup(model1, model2).arrange(RIGHT, buff=1.5).shift(UP * 1.0)
+        
+        frac1 = MathTex(r"\frac{1}{2}", color=BLACK).scale(2.5).next_to(model1, DOWN, buff=0.5)
+        frac2 = MathTex(r"\frac{1}{4}", color=BLACK).scale(2.5).next_to(model2, DOWN, buff=0.5)
+        
+        bottom_text1 = Text("Payda buyudukce kesir kuculur", color=BLACK, weight=BOLD).scale(0.85).to_edge(DOWN, buff=3.5)
+        bottom_text2 = MathTex(r"\frac{1}{2} > \frac{1}{4}", color=BLACK).scale(2.5).to_edge(DOWN, buff=3.5)
+        
+        self.play(Create(c1_outline), Create(c1_line))
+        self.play(FadeIn(c1_sector), Write(frac1))
         self.wait(2.3)
         
-        line1 = Line(pizza1.get_top(), pizza1.get_bottom(), color=BLACK)
-        line2_v = Line(pizza2.get_top(), pizza2.get_bottom(), color=BLACK)
-        line2_h = Line(pizza2.get_left(), pizza2.get_right(), color=BLACK)
-        self.play(Create(line1), Create(line2_v), Create(line2_h))
-        self.wait(3.0)
+        self.play(Create(c2_outline), Create(c2_line1), Create(c2_line2))
+        self.play(FadeIn(c2_sector), Write(frac2))
+        self.wait(1.7)
         
-        slice1 = Sector(radius=2.5, angle=PI, start_angle=PI/2, color=RED, fill_opacity=0.8).shift(UP * 1.5 + LEFT * 3)
-        label1 = MathTex(r"\frac{1}{2}", color=BLACK, font_size=72).next_to(pizza1, DOWN, buff=0.5)
-        self.play(Create(slice1), Write(label1))
-        self.wait(3.3)
+        self.play(Write(bottom_text1))
+        self.wait(2.0)
         
-        slice2 = Sector(radius=2.5, angle=PI/2, start_angle=PI/2, color=BLUE, fill_opacity=0.8).shift(UP * 1.5 + RIGHT * 3)
-        label2 = MathTex(r"\frac{1}{4}", color=BLACK, font_size=72).next_to(pizza2, DOWN, buff=0.5)
-        self.play(Create(slice2), Write(label2))
-        self.wait(3.0)
-        
-        inequality = MathTex(">", color=BLACK, font_size=96).move_to(UP * 1.5)
-        self.play(Write(inequality))
-        self.play(Indicate(slice1, color=RED), Indicate(slice2, color=BLUE))
-        self.wait(4.3)
-        
-        result_text = Text("Payda büyüdükçe kesir küçülür!", color=BLACK, weight=BOLD).to_edge(DOWN, buff=3.0)
-        self.play(Write(result_text))
-        self.wait(6.6)
-        
-        self.wait(4.3)
+        self.play(ReplacementTransform(bottom_text1, bottom_text2))
+        self.wait(5.0)
