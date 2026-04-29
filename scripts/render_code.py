f00ea752b6d1769275aa2f09396016eebab9b436
soykan8-maarifmatematik <soykan8@gmail.com>
2026-04-29 23:19:11 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -3,47 +3,40 @@ from manim import *
 config.pixel_height = 1920
 config.pixel_width = 1080
 
-class BirimKesirler(Scene):
+class MaarifScene(Scene):
     def construct(self):
         self.camera.background_color = "#FFFFFF"
-
-        title = Text("BIRIM KESIRLER", color="#333333", font_size=60).to_edge(UP, buff=1.5)
+        
+        title = Text("BİRİM KESİRLER", color=BLACK, weight=BOLD, font_size=72).to_edge(UP, buff=1.5)
         self.play(Write(title))
-        self.wait(3.6)
-
-        circle1 = Circle(radius=1.0, color="#333333", stroke_width=4)
-        circle2 = Circle(radius=1.0, color="#333333", stroke_width=4)
-        circle3 = Circle(radius=1.0, color="#333333", stroke_width=4)
-        circles = VGroup(circle1, circle2, circle3).arrange(RIGHT, buff=0.5).shift(UP * 1.5)
-
-        self.play(Create(circles))
-        self.wait(2.0)
-
-        slice1 = Sector(outer_radius=1.0, angle=PI, color="#007BFF", fill_opacity=0.8).move_to(circle1)
-        frac1 = MathTex(r"\frac{1}{2}", color="#333333", font_size=72).next_to(circle1, DOWN, buff=0.5)
-        self.play(Create(slice1), Write(frac1))
-        self.wait(4.6)
-
-        slice2 = Sector(outer_radius=1.0, angle=2*PI/3, color="#007BFF", fill_opacity=0.8).move_to(circle2)
-        frac2 = MathTex(r"\frac{1}{3}", color="#333333", font_size=72).next_to(circle2, DOWN, buff=0.5)
-        self.play(Create(slice2), Write(frac2))
-        self.wait(4.6)
-
-        slice3 = Sector(outer_radius=1.0, angle=PI/2, color="#007BFF", fill_opacity=0.8).move_to(circle3)
-        frac3 = MathTex(r"\frac{1}{4}", color="#333333", font_size=72).next_to(circle3, DOWN, buff=0.5)
-        self.play(Create(slice3), Write(frac3))
-        self.wait(3.6)
-
-        self.play(Indicate(slice1, color=RED, scale_factor=1.1))
-        self.wait(3.6)
-
-        final_text = MarkupText(
-            "<span fgcolor='#007BFF'>Payda</span> büyüdükçe,\n<span fgcolor='RED'>dilim</span> küçülür!",
-            color="#333333", font_size=55, justify=True
-        ).to_edge(DOWN, buff=3.0)
-
-        self.play(Write(final_text))
-        self.wait(5.6)
-
-        self.play(Circumscribe(frac1, color=RED, time_width=2))
-        self.wait(7.3)
+        self.wait(1.5)
+        self.wait(4.5)
+        
+        circle1 = Circle(radius=1.5, color=BLACK, stroke_width=4).shift(UP * 1.0 + LEFT * 2.2)
+        slice1 = Sector(radius=1.5, angle=PI, color=ORANGE, fill_opacity=0.8).shift(UP * 1.0 + LEFT * 2.2)
+        text1 = MathTex(r"\frac{1}{2}", color=BLACK, font_size=72).next_to(circle1, DOWN, buff=0.5)
+        
+        circle2 = Circle(radius=1.5, color=BLACK, stroke_width=4).shift(UP * 1.0 + RIGHT * 2.2)
+        slice2 = Sector(radius=1.5, angle=PI/4, color=BLUE, fill_opacity=0.8).shift(UP * 1.0 + RIGHT * 2.2)
+        text2 = MathTex(r"\frac{1}{8}", color=BLACK, font_size=72).next_to(circle2, DOWN, buff=0.5)
+        
+        self.play(Create(circle1), Create(circle2))
+        self.wait(2.5)
+        
+        self.play(Create(slice1), Write(text1))
+        self.wait(4.5)
+        
+        self.play(Create(slice2), Write(text2))
+        self.wait(5.5)
+        
+        greater_sign = MathTex(">", color=RED, font_size=120).shift(UP * 1.0)
+        self.play(Write(greater_sign))
+        self.wait(2.5)
+        
+        result_text = Text("Payda büyüdükçe\nkesir küçülür!", color=BLACK, weight=BOLD, font_size=56, text_alignment="CENTER").to_edge(DOWN, buff=3.0)
+        self.play(Write(result_text))
+        self.wait(5.5)
+        self.wait(2.5)
+        self.wait(2.5)
+        
+        self.play(FadeOut(Group(title, circle1, slice1, text1, circle2, slice2, text2, greater_sign, result_text)))
