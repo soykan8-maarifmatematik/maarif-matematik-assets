df1579fc06f4f03832c021e717af8aa602ab5b80
soykan8-maarifmatematik <soykan8@gmail.com>
2026-04-29 13:40:48 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -2,48 +2,51 @@ from manim import *
 config.pixel_height = 1920
 config.pixel_width = 1080
 
-class BirimKesirler(Scene):
+class UnitFractions(Scene):
     def construct(self):
         self.camera.background_color = "#FFFFFF"
-
-        # Baslik
-        title = Text("BİRİM KESİRLER", color=BLACK, weight=BOLD, font_size=60)
+        
+        title = Text("BİRİM KESİRLER", color=BLACK, weight=BOLD, font_size=70)
         title.to_edge(UP, buff=1.0)
-
-        # Intro (11 kelime)
-        self.play(Write(title))
-        self.wait(11 / 3.0)
-
-        # Modeller
+        
         circle1 = Circle(radius=1.5, color=BLACK)
-        sector1 = Sector(radius=1.5, angle=PI, color=RED, fill_opacity=0.8).rotate(-PI/2)
-        frac1 = MathTex(r"\frac{1}{2}", color=BLACK, font_size=80).next_to(circle1, DOWN, buff=0.5)
-        group1 = VGroup(circle1, sector1, frac1)
-
+        slice1 = Sector(radius=1.5, angle=PI, start_angle=0, color=RED, fill_opacity=0.8)
+        label1 = MathTex(r"\frac{1}{2}", color=BLACK, font_size=70).next_to(circle1, LEFT, buff=0.8)
+        group1 = VGroup(circle1, slice1, label1)
+        
         circle2 = Circle(radius=1.5, color=BLACK)
-        sector2 = Sector(radius=1.5, angle=PI/2, color=BLUE, fill_opacity=0.8).rotate(-PI/2)
-        frac2 = MathTex(r"\frac{1}{4}", color=BLACK, font_size=80).next_to(circle2, DOWN, buff=0.5)
-        group2 = VGroup(circle2, sector2, frac2)
-
-        models = VGroup(group1, group2).arrange(RIGHT, buff=1.0).move_to(ORIGIN)
-
-        # 1/2 kismi (14 kelime)
-        self.play(Create(circle1), FadeIn(sector1), Write(frac1))
-        self.wait(14 / 3.0)
-
-        # 1/4 kismi (11 kelime)
-        self.play(Create(circle2), FadeIn(sector2), Write(frac2))
-        self.wait(11 / 3.0)
-
-        # Karsilastirma (17 kelime)
-        greater_sign = MathTex(">", color=BLACK, font_size=100).move_to(ORIGIN).shift(UP*0.5)
-        self.play(Write(greater_sign))
-        self.wait(17 / 3.0)
-
-        # Alt Metin
-        result_text = Text("Payda büyüdükçe kesir KÜÇÜLÜR!", color=RED, weight=BOLD, font_size=50)
-        result_text.to_edge(DOWN, buff=2.0)
-
-        # Sonuc (8 kelime)
-        self.play(Write(result_text))
+        slice2 = Sector(radius=1.5, angle=PI/2, start_angle=0, color=BLUE, fill_opacity=0.8)
+        label2 = MathTex(r"\frac{1}{4}", color=BLACK, font_size=70).next_to(circle2, LEFT, buff=0.8)
+        group2 = VGroup(circle2, slice2, label2)
+        
+        pizzas = VGroup(group1, group2).arrange(DOWN, buff=1.5).move_to(ORIGIN)
+        
+        result = Text("Payda büyüdükçe kesir küçülür!", color=BLACK, weight=BOLD, font_size=50)
+        result.to_edge(DOWN, buff=2.0)
+        
+        self.play(Write(title))
+        self.wait(7 / 3.0)
+        
+        self.wait(4 / 3.0)
+        
+        self.play(Create(circle1), Create(circle2))
+        self.wait(5 / 3.0)
+        
+        line1 = Line(circle1.get_top(), circle1.get_bottom(), color=BLACK)
+        self.play(Create(line1))
+        self.play(FadeIn(slice1), Write(label1))
+        self.wait(13 / 3.0)
+        
+        line2_v = Line(circle2.get_top(), circle2.get_bottom(), color=BLACK)
+        line2_h = Line(circle2.get_left(), circle2.get_right(), color=BLACK)
+        self.play(Create(line2_v), Create(line2_h))
+        self.play(FadeIn(slice2), Write(label2))
+        self.wait(15 / 3.0)
+        
+        self.play(Indicate(slice1, color=RED, scale_factor=1.1), Indicate(slice2, color=BLUE, scale_factor=1.1))
+        self.wait(13 / 3.0)
+        
+        self.play(Write(result))
+        self.wait(7 / 3.0)
+        
         self.wait(8 / 3.0)
