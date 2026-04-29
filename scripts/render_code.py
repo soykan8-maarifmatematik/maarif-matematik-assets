cc6a174644fb8c58480e57812e9d741b2b9e2f53
soykan8-maarifmatematik <soykan8@gmail.com>
2026-04-29 13:44:07 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -1,4 +1,5 @@
 from manim import *
+
 config.pixel_height = 1920
 config.pixel_width = 1080
 
@@ -6,47 +7,57 @@ class UnitFractions(Scene):
     def construct(self):
         self.camera.background_color = "#FFFFFF"
         
-        title = Text("BİRİM KESİRLER", color=BLACK, weight=BOLD, font_size=70)
-        title.to_edge(UP, buff=1.0)
-        
-        circle1 = Circle(radius=1.5, color=BLACK)
-        slice1 = Sector(radius=1.5, angle=PI, start_angle=0, color=RED, fill_opacity=0.8)
-        label1 = MathTex(r"\frac{1}{2}", color=BLACK, font_size=70).next_to(circle1, LEFT, buff=0.8)
-        group1 = VGroup(circle1, slice1, label1)
-        
-        circle2 = Circle(radius=1.5, color=BLACK)
-        slice2 = Sector(radius=1.5, angle=PI/2, start_angle=0, color=BLUE, fill_opacity=0.8)
-        label2 = MathTex(r"\frac{1}{4}", color=BLACK, font_size=70).next_to(circle2, LEFT, buff=0.8)
-        group2 = VGroup(circle2, slice2, label2)
-        
-        pizzas = VGroup(group1, group2).arrange(DOWN, buff=1.5).move_to(ORIGIN)
-        
-        result = Text("Payda büyüdükçe kesir küçülür!", color=BLACK, weight=BOLD, font_size=50)
-        result.to_edge(DOWN, buff=2.0)
-        
+        # BAŞLIK
+        title = Text("BİRİM KESİRLER", color=BLACK, weight=BOLD).to_edge(UP, buff=1.0)
         self.play(Write(title))
-        self.wait(7 / 3.0)
+        self.wait(3.3)
         
-        self.wait(4 / 3.0)
+        # GİRİŞ METNİ
+        intro_text = Text("Payı 1 olan kesir", color=BLUE, weight=BOLD).next_to(title, DOWN, buff=0.5)
+        self.play(FadeIn(intro_text))
+        self.wait(4.0)
+        self.play(FadeOut(intro_text))
         
-        self.play(Create(circle1), Create(circle2))
-        self.wait(5 / 3.0)
+        # ANA ŞEKİLLER (Merkezde)
+        pizza1 = Circle(radius=1.5, color=BLACK).shift(LEFT * 2.2)
+        pizza2 = Circle(radius=1.5, color=BLACK).shift(RIGHT * 2.2)
         
-        line1 = Line(circle1.get_top(), circle1.get_bottom(), color=BLACK)
-        self.play(Create(line1))
-        self.play(FadeIn(slice1), Write(label1))
-        self.wait(13 / 3.0)
+        self.play(Create(pizza1), Create(pizza2))
+        self.wait(2.3)
         
-        line2_v = Line(circle2.get_top(), circle2.get_bottom(), color=BLACK)
-        line2_h = Line(circle2.get_left(), circle2.get_right(), color=BLACK)
-        self.play(Create(line2_v), Create(line2_h))
-        self.play(FadeIn(slice2), Write(label2))
-        self.wait(15 / 3.0)
+        # BÖLME ÇİZGİLERİ
+        line1 = Line(pizza1.get_top(), pizza1.get_bottom(), color=BLACK)
+        lines2 = VGroup(
+            Line(pizza2.get_top(), pizza2.get_bottom(), color=BLACK),
+            Line(pizza2.get_left(), pizza2.get_right(), color=BLACK)
+        )
+        self.play(Create(line1), Create(lines2))
+        self.wait(2.3)
         
-        self.play(Indicate(slice1, color=RED, scale_factor=1.1), Indicate(slice2, color=BLUE, scale_factor=1.1))
-        self.wait(13 / 3.0)
+        # DİLİMLER (Sadece radius kullanıldı, move_to ile hizalandı)
+        slice1 = Sector(radius=1.5, angle=PI, start_angle=PI/2, color=BLUE, fill_opacity=0.6).move_to(pizza1.get_center())
+        slice2 = Sector(radius=1.5, angle=PI/2, start_angle=PI/2, color=RED, fill_opacity=0.6).move_to(pizza2.get_center())
         
-        self.play(Write(result))
-        self.wait(7 / 3.0)
+        self.play(FadeIn(slice1), FadeIn(slice2))
+        self.wait(4.3)
         
-        self.wait(8 / 3.0)
+        # KESİR YAZILARI VE İŞARET
+        label1 = MathTex("\\frac{1}{2}", color=BLACK, font_size=72).next_to(pizza1, DOWN, buff=0.5)
+        label2 = MathTex("\\frac{1}{4}", color=BLACK, font_size=72).next_to(pizza2, DOWN, buff=0.5)
+        sign = MathTex(">", color=BLACK, font_size=96).move_to(ORIGIN)
+        
+        self.play(Write(label1), Write(label2), Write(sign))
+        self.wait(4.6)
+        
+        # ALT METİN (Sonuç)
+        rule_text1 = Text("Payda Büyüdükçe", color=RED, weight=BOLD)
+        rule_text2 = Text("Kesir KÜÇÜLÜR!", color=BLUE, weight=BOLD)
+        rule_group = VGroup(rule_text1, rule_text2).arrange(DOWN, buff=0.3).to_edge(DOWN, buff=2.0)
+        
+        self.play(Write(rule_group))
+        self.wait(2.6)
+        
+        # KAPANIŞ
+        outro = Text("Maarif Matematik", color=DARK_GRAY, weight=BOLD).next_to(rule_group, UP, buff=1.0)
+        self.play(FadeIn(outro))
+        self.wait(2.3)
