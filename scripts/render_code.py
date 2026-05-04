bb7182487dfb732e9e988c437a58b1130be439a0
soykan8-maarifmatematik <soykan8@gmail.com>
2026-05-04 16:08:14 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -1,5 +1,4 @@
 from manim import *
-import numpy as np
 
 config.pixel_height = 1920
 config.pixel_width = 1080
@@ -8,35 +7,39 @@ config.frame_width = 8.0
 
 class MaarifScene(Scene):
     def construct(self):
-        title = Text("Birim Kesirlerin Büyüklüğü").to_edge(np.array([0,1,0]), buff=1.2).scale_to_fit_width(6.0)
-        self.play(Write(title))
-
-        sector_half = Sector(outer_radius=0.9, angle=PI, color=BLUE, fill_opacity=0.7)
-        circle_half = Circle(radius=0.9, color=WHITE)
-        line_half = Line(np.array([-0.9, 0, 0]), np.array([0.9, 0, 0]), color=WHITE)
-        group_half = VGroup(sector_half, circle_half, line_half)
-
-        sector_quarter = Sector(outer_radius=0.9, angle=PI/2, color=RED, fill_opacity=0.7)
-        circle_quarter = Circle(radius=0.9, color=WHITE)
-        line_q1 = Line(np.array([-0.9, 0, 0]), np.array([0.9, 0, 0]), color=WHITE)
-        line_q2 = Line(np.array([0, -0.9, 0]), np.array([0, 0.9, 0]), color=WHITE)
-        group_quarter = VGroup(sector_quarter, circle_quarter, line_q1, line_q2)
-
-        models = VGroup(group_half, group_quarter).arrange(np.array([1,0,0]), buff=1.5)
-        models.scale(0.8).shift(np.array([0,1.5,0]))
-
-        self.play(Create(sector_half))
-        self.play(Create(circle_half))
-        self.play(Create(line_half))
-
-        self.play(Create(sector_quarter))
-        self.play(Create(circle_quarter))
-        self.play(Create(line_q1), Create(line_q2))
-
-        result_text = Text("Payda büyüdükçe parça küçülür!").to_edge(np.array([0,-1,0]), buff=4.8).scale_to_fit_width(6.0)
-        self.play(Write(result_text))
-
-        math_result = MathTex(r"\frac{1}{2} > \frac{1}{4}").next_to(result_text, np.array([0,-1,0]), buff=1.0).scale_to_fit_width(6.0)
-        self.play(Write(math_result))
+        self.camera.background_color = "#FFFFFF"
         
+        title = Text("Birim Kesirlerin Büyüklüğü", color=BLACK, weight=BOLD)
+        title.to_edge(np.array([0,1,0]), buff=1.2)
+        title.scale_to_fit_width(6.0)
+        
+        c1 = Circle(radius=0.9, color=BLACK)
+        s1 = Sector(radius=0.9, angle=PI, color=BLUE, fill_opacity=0.8)
+        t1 = MathTex("\\frac{1}{2}", color=BLACK).shift(np.array([0,-1.5,0]))
+        g1 = VGroup(c1, s1, t1)
+        
+        c2 = Circle(radius=0.9, color=BLACK)
+        s2 = Sector(radius=0.9, angle=2*PI/3, color=RED, fill_opacity=0.8)
+        t2 = MathTex("\\frac{1}{3}", color=BLACK).shift(np.array([0,-1.5,0]))
+        g2 = VGroup(c2, s2, t2)
+        
+        c3 = Circle(radius=0.9, color=BLACK)
+        s3 = Sector(radius=0.9, angle=PI/2, color=GREEN, fill_opacity=0.8)
+        t3 = MathTex("\\frac{1}{4}", color=BLACK).shift(np.array([0,-1.5,0]))
+        g3 = VGroup(c3, s3, t3)
+        
+        models = VGroup(g1, g2, g3)
+        models.arrange(np.array([1,0,0]), buff=0.8)
+        models.scale(0.8)
+        models.shift(np.array([0,1.5,0]))
+        
+        result_text = Text("Payda büyüdükçe parça küçülür!", color=BLACK)
+        result_text.to_edge(np.array([0,-1,0]), buff=4.8)
+        result_text.scale_to_fit_width(6.0)
+        
+        self.play(Write(title))
+        self.play(Create(g1))
+        self.play(Create(g2))
+        self.play(Create(g3))
+        self.play(Write(result_text))
         self.wait(2)
