846853da3c4cfe121682bcef01ce83ae469e66f0
soykan8-maarifmatematik <soykan8@gmail.com>
2026-05-04 16:21:09 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -1,4 +1,5 @@
 from manim import *
+import numpy as np
 
 config.pixel_height = 1920
 config.pixel_width = 1080
@@ -8,38 +9,51 @@ config.frame_width = 8.0
 class MaarifScene(Scene):
     def construct(self):
         self.camera.background_color = "#FFFFFF"
-        
-        title = Text("Birim Kesirlerin Büyüklüğü", color=BLACK, weight=BOLD)
-        title.to_edge(np.array([0,1,0]), buff=1.2)
-        title.scale_to_fit_width(6.0)
-        
-        c1 = Circle(radius=0.9, color=BLACK)
-        s1 = Sector(radius=0.9, angle=PI, color=BLUE, fill_opacity=0.8)
-        t1 = MathTex("\\frac{1}{2}", color=BLACK).shift(np.array([0,-1.5,0]))
-        g1 = VGroup(c1, s1, t1)
-        
-        c2 = Circle(radius=0.9, color=BLACK)
-        s2 = Sector(radius=0.9, angle=2*PI/3, color=RED, fill_opacity=0.8)
-        t2 = MathTex("\\frac{1}{3}", color=BLACK).shift(np.array([0,-1.5,0]))
-        g2 = VGroup(c2, s2, t2)
-        
-        c3 = Circle(radius=0.9, color=BLACK)
-        s3 = Sector(radius=0.9, angle=PI/2, color=GREEN, fill_opacity=0.8)
-        t3 = MathTex("\\frac{1}{4}", color=BLACK).shift(np.array([0,-1.5,0]))
-        g3 = VGroup(c3, s3, t3)
-        
-        models = VGroup(g1, g2, g3)
-        models.arrange(np.array([1,0,0]), buff=0.8)
-        models.scale(0.8)
-        models.shift(np.array([0,1.5,0]))
-        
-        result_text = Text("Payda büyüdükçe parça küçülür!", color=BLACK)
-        result_text.to_edge(np.array([0,-1,0]), buff=4.8)
-        result_text.scale_to_fit_width(6.0)
-        
+
+        # Başlık
+        title = Text("BİRİM KESİRLERİ KARŞILAŞTIRMA", color=BLACK, font_size=48).scale_to_fit_width(6.0)
+        title.to_edge(np.array([0, 1, 0]), buff=1.2)
         self.play(Write(title))
-        self.play(Create(g1))
-        self.play(Create(g2))
-        self.play(Create(g3))
+
+        # 1/2 Modeli
+        circle1 = Circle(radius=0.9, color=BLACK, stroke_width=4)
+        sector1 = Sector(radius=0.9, angle=PI, start_angle=0, color=BLUE, fill_opacity=0.7)
+        line1_1 = Line(start=np.array([-0.9, 0, 0]), end=np.array([0.9, 0, 0]), color=BLACK, stroke_width=4)
+        
+        frac1_group = VGroup(circle1, sector1, line1_1)
+        frac1_group.shift(np.array([-2.5, 0, 0]))
+        label1 = MathTex(r"\frac{1}{2}", color=BLACK).scale(1.5)
+        label1.next_to(frac1_group, np.array([0, -1, 0]), buff=0.5)
+
+        # 1/4 Modeli
+        circle2 = Circle(radius=0.9, color=BLACK, stroke_width=4)
+        sector2 = Sector(radius=0.9, angle=PI/2, start_angle=0, color=RED, fill_opacity=0.7)
+        line2_1 = Line(start=np.array([-0.9, 0, 0]), end=np.array([0.9, 0, 0]), color=BLACK, stroke_width=4)
+        line2_2 = Line(start=np.array([0, -0.9, 0]), end=np.array([0, 0.9, 0]), color=BLACK, stroke_width=4)
+        
+        frac2_group = VGroup(circle2, sector2, line2_1, line2_2)
+        frac2_group.shift(np.array([2.5, 0, 0]))
+        label2 = MathTex(r"\frac{1}{4}", color=BLACK).scale(1.5)
+        label2.next_to(frac2_group, np.array([0, -1, 0]), buff=0.5)
+
+        # Karşılaştırma Sembolü
+        symbol = MathTex(">", color=BLACK).scale(2.5)
+        symbol.move_to(np.array([0, 0, 0]))
+
+        # Modelleri Gruplama ve Konumlandırma
+        models_group = VGroup(frac1_group, label1, frac2_group, label2, symbol)
+        models_group.scale(0.8).shift(np.array([0, 1.5, 0]))
+
+        # Animasyonlar
+        self.play(Create(circle1), Create(circle2))
+        self.play(Create(line1_1), Create(line2_1), Create(line2_2))
+        self.play(Create(sector1), Create(sector2))
+        self.play(Write(label1), Write(label2))
+        self.play(Write(symbol))
+
+        # Sonuç Metni
+        result_text = Text("Payda büyüdükçe dilim küçülür!", color=BLACK, font_size=40).scale_to_fit_width(6.0)
+        result_text.to_edge(np.array([0, -1, 0]), buff=4.8)
         self.play(Write(result_text))
+        
         self.wait(2)
