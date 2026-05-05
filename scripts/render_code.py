5f53dd25227b33702bd633ce7fc52e8cb1cc3046
soykan8-maarifmatematik <soykan8@gmail.com>
2026-05-05 13:45:18 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -4,54 +4,50 @@ config.pixel_height = 1920
 config.pixel_width = 1080
 config.frame_height = 14.22
 config.frame_width = 8.0
+config.background_color = "#FFFFFF"
 
 class MaarifScene(Scene):
     def construct(self):
-        self.camera.background_color = "#FFFFFF"
-
-        # BAŞLIK
+        # Başlık
         title = Text("BİRİM KESİRLERİ KARŞILAŞTIRMA", font="Montserrat", weight=BOLD, color="#212121")
         title.scale_to_fit_width(7.0)
         title.to_edge(np.array([0, 1, 0]), buff=1.0)
         self.play(Write(title))
 
-        # MODELLER
-        # Sol Model: 1/2
-        left_circle = Circle(radius=0.9, color="#212121")
-        left_filled = Sector(radius=0.9, angle=PI, start_angle=PI/2, color="#FF5722", fill_opacity=0.8)
-        left_line = Line(start=np.array([0, 0.9, 0]), end=np.array([0, -0.9, 0]), color="#212121")
-        left_label = Text("1/2", font="Montserrat", color="#212121")
-        left_label.next_to(left_circle, np.array([0, -1, 0]))
-        left_group = VGroup(left_filled, left_circle, left_line, left_label)
-        left_group.move_to(np.array([-2.5, 0, 0]))
+        # Kesirler
+        frac1 = MathTex(r"\frac{1}{3}", color="#212121").scale(2.5)
+        frac2 = MathTex(r"\frac{1}{5}", color="#212121").scale(2.5)
 
-        # Sağ Model: 1/4
-        right_circle = Circle(radius=0.9, color="#212121")
-        right_filled = Sector(radius=0.9, angle=PI/2, start_angle=PI/2, color="#2196F3", fill_opacity=0.8)
-        right_line1 = Line(start=np.array([0, 0.9, 0]), end=np.array([0, -0.9, 0]), color="#212121")
-        right_line2 = Line(start=np.array([-0.9, 0, 0]), end=np.array([0.9, 0, 0]), color="#212121")
-        right_label = Text("1/4", font="Montserrat", color="#212121")
-        right_label.next_to(right_circle, np.array([0, -1, 0]))
-        right_group = VGroup(right_filled, right_circle, right_line1, right_line2, right_label)
-        right_group.move_to(np.array([2.5, 0, 0]))
+        # 1/3 Modeli
+        circle1 = Circle(radius=0.9, color="#212121")
+        slice1 = Sector(radius=0.9, angle=TAU/3, color=BLUE, fill_opacity=0.7)
+        lines1 = VGroup(*[Line(ORIGIN, np.array([0.9*np.cos(i*TAU/3), 0.9*np.sin(i*TAU/3), 0]), color="#212121") for i in range(3)])
+        model1 = VGroup(circle1, slice1, lines1)
+
+        # 1/5 Modeli
+        circle2 = Circle(radius=0.9, color="#212121")
+        slice2 = Sector(radius=0.9, angle=TAU/5, color=RED, fill_opacity=0.7)
+        lines2 = VGroup(*[Line(ORIGIN, np.array([0.9*np.cos(i*TAU/5), 0.9*np.sin(i*TAU/5), 0]), color="#212121") for i in range(5)])
+        model2 = VGroup(circle2, slice2, lines2)
+
+        # Gruplama ve Yerleşim
+        group1 = VGroup(frac1, model1).arrange(np.array([0, -1, 0]), buff=0.8)
+        group2 = VGroup(frac2, model2).arrange(np.array([0, -1, 0]), buff=0.8)
+
+        comparison_group = VGroup(group1, group2).arrange(np.array([1, 0, 0]), buff=1.5)
+        comparison_group.scale(0.8).shift(np.array([0, 1.0, 0]))
+        
+        self.play(Create(comparison_group))
 
         # Sembol
-        symbol = Text(">", font="Montserrat", color="#212121").scale(2.0)
-        symbol.move_to(ORIGIN)
-
-        # Model ve Sembol Gruplaması (Ölçek ve Konumlandırma)
-        models_group = VGroup(left_group, symbol, right_group)
-        models_group.scale(0.8)
-        models_group.shift(np.array([0, 1.5, 0]))
-
-        self.play(Create(left_group))
-        self.play(Create(right_group))
+        symbol = MathTex(">", color="#212121").scale(3)
+        symbol.move_to(comparison_group.get_center())
         self.play(Write(symbol))
 
-        # SONUÇ METNİ
-        result_text = Text("Payda büyüdükçe, dilim küçülür!", font="Montserrat", weight=BOLD, color="#212121")
+        # Sonuç Metni
+        result_text = Text("Payda küçüldükçe parça büyür!", font="Montserrat", weight=BOLD, color="#212121")
         result_text.scale_to_fit_width(6.0)
         result_text.to_edge(np.array([0, -1, 0]), buff=4.8)
         self.play(Write(result_text))
-
+        
         self.wait(2)
