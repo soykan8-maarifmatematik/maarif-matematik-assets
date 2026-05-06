401e5c22b21fdbdafab095cec3d0e4b911653f11
soykan8-maarifmatematik <soykan8@gmail.com>
2026-05-06 16:39:16 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -2,86 +2,52 @@ from manim import *
 
 config.pixel_height, config.pixel_width = 1920, 1080
 
-class BirimKesirler(Scene):
+class UnitFractions(Scene):
     def construct(self):
+        self.camera.background_color = WHITE
         self.camera.frame_width = 9
         self.camera.frame_height = 16
 
-        # ÜST BÖLGE (Başlık)
-        title = Text("BİRİM KESİRLERİ KARŞILAŞTIRMA")
-        title.scale_to_fit_width(6.2)
-        title.to_edge(UP, buff=1.0)
+        header = Text("BİRİM KESİRLER", color="#212121")
+        header.scale_to_fit_width(6.5)
+        header.to_edge(UP, buff=1.0)
 
-        # ORTA-ÜST BÖLGE (Modeller)
-        # 1/3 Modeli
-        sectors_3 = VGroup()
-        for i in range(3):
-            fill_opacity = 0.5 if i == 0 else 0
-            fill_color = BLUE if i == 0 else BLACK
-            sector = Sector(
-                radius=1.0, 
-                angle=TAU/3, 
-                start_angle=i*TAU/3, 
-                stroke_color=WHITE, 
-                stroke_width=2, 
-                fill_color=fill_color, 
-                fill_opacity=fill_opacity
-            )
-            sectors_3.add(sector)
+        circle1_base = Circle(radius=1.0, color="#212121", stroke_width=2)
+        slice1 = Sector(radius=1.0, angle=PI, color=BLUE, fill_opacity=0.8)
+        label1 = MathTex(r"\frac{1}{2}", color="#212121").next_to(circle1_base, DOWN)
+        model1 = VGroup(circle1_base, slice1, label1)
+
+        circle2_base = Circle(radius=1.0, color="#212121", stroke_width=2)
+        slice2 = Sector(radius=1.0, angle=PI/2, color=RED, fill_opacity=0.8)
+        label2 = MathTex(r"\frac{1}{4}", color="#212121").next_to(circle2_base, DOWN)
+        model2 = VGroup(circle2_base, slice2, label2)
+
+        comp_sym = MathTex(">", color="#212121", font_size=72)
+
+        model1.move_to(LEFT * 2)
+        model2.move_to(RIGHT * 2)
+        comp_sym.move_to(ORIGIN)
+
+        group = VGroup(model1, comp_sym, model2)
+        group.move_to(UP * 2.5)
+
+        para = Paragraph("Payda büyüdükçe", "kesrin değeri küçülür.", alignment="center", color="#212121")
+        para.scale_to_fit_width(6.5)
+        para.move_to(DOWN * 3.0)
+
+        self.play(Write(header))
+        self.wait(0.5)
         
-        label_3 = MathTex(r"\frac{1}{3}", font_size=64).next_to(sectors_3, DOWN, buff=0.5)
-        group_3 = VGroup(sectors_3, label_3)
-
-        # 1/4 Modeli
-        sectors_4 = VGroup()
-        for i in range(4):
-            fill_opacity = 0.5 if i == 0 else 0
-            fill_color = RED if i == 0 else BLACK
-            sector = Sector(
-                radius=1.0, 
-                angle=TAU/4, 
-                start_angle=i*TAU/4, 
-                stroke_color=WHITE, 
-                stroke_width=2, 
-                fill_color=fill_color, 
-                fill_opacity=fill_opacity
-            )
-            sectors_4.add(sector)
+        self.play(Create(circle1_base), Create(circle2_base))
+        self.play(Write(label1), Write(label2))
+        self.wait(0.5)
         
-        label_4 = MathTex(r"\frac{1}{4}", font_size=64).next_to(sectors_4, DOWN, buff=0.5)
-        group_4 = VGroup(sectors_4, label_4)
-
-        # Sembol
-        greater_sign = MathTex(">", font_size=72)
-
-        # Modelleri Gruplama ve Konumlandırma
-        models_group = VGroup(group_3, greater_sign, group_4).arrange(RIGHT, buff=0.8)
-        models_group.move_to(ORIGIN).shift(UP * 3.8)
-
-        # ALT-ORTA BÖLGE (Paragraf)
-        paragraph = Paragraph(
-            "Payda büyüdükçe",
-            "dilimler küçülür!",
-            "",
-            "Bu yüzden:",
-            "1/3 > 1/4",
-            alignment="center"
-        )
-        paragraph.scale_to_fit_width(6.2)
-        paragraph.to_edge(DOWN, buff=4.8)
-
-        # Animasyonlar
-        self.play(Write(title))
+        self.play(Create(slice1))
+        self.play(Create(slice2))
         self.wait(0.5)
-
-        self.play(Create(sectors_3), Write(label_3))
-        self.wait(0.5)
-
-        self.play(Create(sectors_4), Write(label_4))
-        self.wait(0.5)
-
-        self.play(Write(greater_sign))
+        
+        self.play(Write(comp_sym))
         self.wait(1)
-
-        self.play(Write(paragraph))
+        
+        self.play(Write(para))
         self.wait(2)
