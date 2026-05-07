a42382dd71ca60f51514e57531f67dd67d355b7b
soykan8-maarifmatematik <soykan8@gmail.com>
2026-05-07 15:49:22 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -8,70 +8,66 @@ class MaarifScene(Scene):
     def construct(self):
         self.camera.background_color = "#000000"
         
-        # V33 - Başlık Kuralı
         header = Paragraph(
             "İKİ BASAMAKLI SAYILARLA\nÇARPMA İŞLEMİ",
             alignment="center",
             line_spacing=0.8,
             color="#FFFFFF",
             weight=BOLD
-        ).scale_to_fit_width(7.0).to_edge(UP, buff=0.9)
+        ).scale_to_fit_width(7.0).to_edge(UP, buff=1.0)
 
-        # V33 - Ölçeklendirme Kuralı (Ana sayılar 1.6)
-        n67 = MathTex("6", "7", color="#FFFFFF").scale(1.6)
-        n89 = MathTex("8", "9", color="#FFFFFF").scale(1.6)
-        sign = MathTex("\\times", color="#FFFFFF").scale(1.6)
-        line1 = Line(LEFT, RIGHT, color="#FFFFFF").scale(1.6)
+        # Ana Sayılar ve İşlem Elemanları (V34 Ölçeklendirme)
+        n83 = MathTex("8", "3", color="#FFFFFF").scale(1.7)
+        n47 = MathTex("4", "7", color="#FFFFFF").scale(1.7)
+        sign = MathTex("\\times", color="#FFFFFF").scale(1.3)
+        line1 = Line(LEFT, RIGHT, color="#FFFFFF").scale(1.5)
 
-        n89.next_to(n67, DOWN, aligned_edge=RIGHT, buff=0.5)
-        sign.next_to(n89, LEFT, buff=0.8)
-        line1.next_to(n89, DOWN, buff=0.2)
+        # Yerleşim: V34 El Yazısı Düzeni (Hafif sola/merkeze çekilmiş)
+        n83.move_to(UP * 1.5 + LEFT * 0.2)
+        n47.next_to(n83, DOWN, aligned_edge=RIGHT, buff=0.5)
+        sign.next_to(n47, LEFT, buff=0.8)
+        line1.next_to(n47, DOWN, buff=0.2)
+
+        # Eldeler (V34 - Ana rakamın hemen üzerinde, scale 0.8)
+        carry_2 = MathTex("+2", color="#FFFF00").scale(0.8).next_to(n83[0], UP, buff=0.1)
+        carry_1 = MathTex("+1", color="#00FFFF").scale(0.8).next_to(n83[0], UP, buff=0.1)
+
+        # Adım Sonuçları
+        res1 = MathTex("5", "8", "1", color="#FFFFFF").scale(1.7).next_to(line1, DOWN, aligned_edge=RIGHT, buff=0.4)
+        res2 = MathTex("3", "3", "2", color="#FFFFFF").scale(1.7).next_to(res1, DOWN, aligned_edge=RIGHT, buff=0.4)
+        res2.shift(LEFT * 0.7) # V34 Zorunlu Basamak Kaydırma
         
-        res1 = MathTex("6", "0", "3", color="#FFFFFF").scale(1.6).next_to(line1, DOWN, aligned_edge=RIGHT, buff=0.3)
-        res2 = MathTex("5", "3", "6", color="#FFFFFF").scale(1.6).next_to(res1, DOWN, aligned_edge=RIGHT, buff=0.3).shift(LEFT * 0.75)
-        
-        line2 = Line(LEFT, RIGHT, color="#FFFFFF").scale(1.6).next_to(res2, DOWN, buff=0.2)
-        final_res = MathTex("5", "9", "6", "3", color="#FFFF00").scale(1.6).next_to(line2, DOWN, aligned_edge=RIGHT, buff=0.3)
+        line2 = Line(LEFT, RIGHT, color="#FFFFFF").scale(1.8).next_to(res2, DOWN, buff=0.2)
+        final_res = MathTex("3", "9", "0", "1", color="#FFFF00").scale(2.0).next_to(line2, DOWN, aligned_edge=RIGHT, buff=0.4)
 
-        # V33 - Merkezleme Kuralı (İşlem grubunu merkezin biraz üstüne alma)
-        math_group = VGroup(n67, n89, sign, line1, res1, res2, line2, final_res)
-        math_group.move_to(ORIGIN + UP * 0.5)
-
-        # V33 - Eldeler Kuralı (Merkezlemeden sonra, scale 0.8 ve buff 0.1)
-        carry_6 = MathTex("+6", color="#FFFF00").scale(0.8).next_to(n67[0], UP, buff=0.1)
-        carry_5 = MathTex("+5", color="#00FFFF").scale(0.8).next_to(n67[0], UP, buff=0.1)
-
-        # V33 - Açıklama Metni Kuralı
         desc = Paragraph(
             "Eldeleri eklemeyi ve ikinci satırı\nsola kaydırmayı unutma!",
             alignment="center",
             color="#FFFFFF"
-        ).to_edge(DOWN, buff=1.5)
+        ).scale_to_fit_width(7.0).to_edge(DOWN, buff=1.2)
 
+        # ANİMASYON (V34 Senkron ve Bekleme Kuralları)
         self.play(Write(header))
-        self.play(Write(n67), Write(n89), Write(sign))
+        self.play(Write(n83, run_time=0.5), Write(n47, run_time=0.5), Write(sign, run_time=0.5))
         self.play(Create(line1))
+        self.wait(1.0)
         
-        # --- 1. SATIR ÇARPIMI (Blok Senkron ve Hızlı Rakamlar) ---
-        self.play(n89[1].animate.set_color("#FFFF00"), run_time=0.3)
-        self.play(Write(res1[2]), run_time=0.3)
-        self.play(Write(carry_6), run_time=0.3)
-        self.play(Write(res1[0:2]), run_time=0.3)
-        self.play(FadeOut(carry_6), n89[1].animate.set_color("#FFFFFF"), run_time=0.3)
-        self.wait(1.5) # V33: Birinci satır bittikten sonra bekleme
+        # 1. Adım
+        self.play(n47[1].animate.set_color("#FFFF00"))
+        self.play(Write(carry_2, run_time=0.5))
+        self.play(Write(res1, run_time=0.8))
+        self.wait(3.0) # V34: 3 Saniye Kuralı
+        self.play(FadeOut(carry_2), n47[1].animate.set_color("#FFFFFF"))
 
-        # --- 2. SATIR ÇARPIMI (Blok Senkron ve Hızlı Rakamlar) ---
-        self.play(n89[0].animate.set_color("#00FFFF"), run_time=0.3)
-        self.play(Write(res2[2]), run_time=0.3)
-        self.play(Write(carry_5), run_time=0.3)
-        self.play(Write(res2[0:2]), run_time=0.3)
-        self.play(FadeOut(carry_5), n89[0].animate.set_color("#FFFFFF"), run_time=0.3)
-        self.wait(1.5) # V33: İkinci satır bittikten sonra bekleme
+        # 2. Adım
+        self.play(n47[0].animate.set_color("#00FFFF"))
+        self.play(Write(carry_1, run_time=0.5))
+        self.play(Write(res2, run_time=0.8))
+        self.wait(3.0) # V34: 3 Saniye Kuralı
+        self.play(FadeOut(carry_1), n47[0].animate.set_color("#FFFFFF"))
 
-        # --- TOPLAMA İŞLEMİ (Blok Senkron) ---
-        self.wait(1.0) # V33: Toplama işlemi başlamadan önce bekleme
-        self.play(Create(line2), run_time=0.3)
-        self.play(Write(final_res), run_time=0.3)
-
+        # Final
+        self.play(Create(line2))
+        self.play(Write(final_res, run_time=0.8))
         self.play(Write(desc))
-        self.wait(3)
+        self.wait(4.0) # V34: Kapanış öncesi 4 saniye bekleme
