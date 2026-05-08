7590659df0d7964fdd99572ff5460c73bdaa9c78
soykan8-maarifmatematik <soykan8@gmail.com>
2026-05-08 16:13:02 +0300

    Maarif Matematik - Yeni Ders Kodu Guncellemesi

--- scripts/render_code.py
+++ scripts/render_code.py
@@ -16,73 +16,81 @@ class MaarifScene(Scene):
             weight=BOLD
         ).scale_to_fit_width(7.0).to_edge(UP, buff=1.1)
 
-        # Ana Sayılar
-        n524 = MathTex('5', '2', '4', color='#FFFFFF').scale(1.7).move_to(UP * 2.5 + LEFT * 0.5)
-        n136 = MathTex('1', '3', '6', color='#FFFFFF').scale(1.7).next_to(n524, DOWN, aligned_edge=RIGHT, buff=0.5)
-        sign_mult = MathTex('\\times', color='#FFFFFF').scale(1.7).next_to(n136, LEFT, buff=0.6)
-        line1 = Line(LEFT, RIGHT, color='#FFFFFF').scale(1.7).next_to(n136, DOWN, buff=0.2)
+        n427 = MathTex('4', '2', '7', color='#FFFFFF').scale(1.7).move_to(UP * 2.5 + LEFT * 0.5)
+        n53 = MathTex('5', '3', color='#FFFFFF').scale(1.7).next_to(n427, DOWN, aligned_edge=RIGHT, buff=0.5)
+        sign_mult = MathTex('\\times', color='#FFFFFF').scale(1.7).next_to(n53, LEFT, buff=0.6)
+        line1 = Line(LEFT, RIGHT, color='#FFFFFF').scale(1.5).next_to(n53, DOWN, buff=0.2)
 
-        # Satırlar ve Hizalama
-        res1 = MathTex('3', '1', '4', '4', color='#FFFFFF').scale(1.7).next_to(line1, DOWN, buff=0.4).align_to(n524[2], RIGHT)
-        res2 = MathTex('1', '5', '7', '2', color='#FFFFFF').scale(1.7).next_to(res1, DOWN, buff=0.4).align_to(n524[1], RIGHT)
-        res3 = MathTex('5', '2', '4', color='#FFFFFF').scale(1.7).next_to(res2, DOWN, buff=0.4).align_to(n524[0], RIGHT)
+        res1 = MathTex('1', '2', '8', '1', color='#FFFFFF').scale(1.7).next_to(line1, DOWN, buff=0.4).align_to(n427[2], RIGHT)
+        res2 = MathTex('2', '1', '3', '5', color='#FFFFFF').scale(1.7).next_to(res1, DOWN, buff=0.4).align_to(n427[1], RIGHT)
 
-        line2 = Line(LEFT, RIGHT, color='#FFFFFF').scale(2.2).next_to(res3, DOWN, buff=0.2).align_to(line1, RIGHT)
+        line2 = Line(LEFT, RIGHT, color='#FFFFFF').scale(2.0).next_to(res2, DOWN, buff=0.2).align_to(line1, RIGHT)
         sign_add = MathTex('+', color='#FFFFFF').scale(1.7).next_to(line2, LEFT, buff=0.3).shift(UP * 0.4)
 
-        final_res = MathTex('7', '1', '2', '6', '4', color='#FFFF00').scale(1.8).next_to(line2, DOWN, buff=0.4).align_to(res1, RIGHT)
+        final_res = MathTex('2', '2', '6', '3', '1', color='#FFFF00').scale(1.8).next_to(line2, DOWN, buff=0.4).align_to(res1, RIGHT)
 
-        # Eldeler (Carries)
-        c2 = MathTex('+2', color='#FFFF00').scale(0.8).next_to(n524[1], UP, buff=0.1)
-        c1_1 = MathTex('+1', color='#FFFF00').scale(0.8).next_to(n524[0], UP, buff=0.1)
-        c1_2 = MathTex('+1', color='#00FFFF').scale(0.8).next_to(n524[1], UP, buff=0.1)
+        c2 = MathTex('+2', color='#FFFF00').scale(0.8).next_to(n427[1], UP, buff=0.1)
+        c3 = MathTex('+3', color='#00FFFF').scale(0.8).next_to(n427[1], UP, buff=0.1)
+        c1 = MathTex('+1', color='#00FFFF').scale(0.8).next_to(n427[0], UP, buff=0.1)
+        c1_add = MathTex('+1', color='#FFFF00').scale(0.8).next_to(res1[1], UP, buff=0.1)
 
         self.play(Write(header))
-        self.play(Write(n524), Write(n136), Write(sign_mult))
+        self.play(Write(n427), Write(n53), Write(sign_mult))
         self.play(Create(line1))
-
-        # x6
-        self.play(n136[2].animate.set_color('#FFFF00'))
         self.wait(1.0)
+
+        # 1. Basamak (3)
+        self.play(n53[1].animate.set_color('#FFFF00'))
+        self.wait(2.0)
         self.play(Write(res1[3]), run_time=0.8)
         self.play(Write(c2), run_time=0.8)
-        self.wait(1.5)
+        self.wait(2.5)
         self.play(Write(res1[2]), run_time=0.8)
-        self.play(FadeOut(c2), Write(c1_1))
-        self.wait(1.5)
+        self.play(FadeOut(c2))
+        self.wait(2.5)
         self.play(Write(res1[1]), run_time=0.8)
         self.play(Write(res1[0]), run_time=0.8)
-        self.play(FadeOut(c1_1))
         self.wait(3.5)
 
-        # x3
-        self.play(n136[2].animate.set_color('#FFFFFF'), n136[1].animate.set_color('#00FFFF'))
-        self.wait(1.0)
+        # 2. Basamak (5)
+        self.play(n53[1].animate.set_color('#FFFFFF'), n53[0].animate.set_color('#00FFFF'))
+        self.wait(3.0)
         self.play(Write(res2[3]), run_time=0.8)
-        self.play(Write(c1_2), run_time=0.8)
-        self.wait(1.5)
+        self.play(Write(c3), run_time=0.8)
+        self.wait(2.5)
         self.play(Write(res2[2]), run_time=0.8)
-        self.play(FadeOut(c1_2))
-        self.wait(1.5)
+        self.play(FadeOut(c3), Write(c1))
+        self.wait(2.5)
         self.play(Write(res2[1]), run_time=0.8)
         self.play(Write(res2[0]), run_time=0.8)
+        self.play(FadeOut(c1))
         self.wait(3.5)
+        self.play(n53[0].animate.set_color('#FFFFFF'))
 
-        # x1
-        self.play(n136[1].animate.set_color('#FFFFFF'), n136[0].animate.set_color('#FFFF00'))
-        self.wait(1.0)
-        self.play(Write(res3[2]), run_time=0.8)
-        self.wait(1.0)
-        self.play(Write(res3[1]), run_time=0.8)
-        self.wait(1.0)
-        self.play(Write(res3[0]), run_time=0.8)
-        self.wait(3.5)
-
-        # Toplama
+        # Toplama İşlemi
         self.play(Create(line2), Write(sign_add))
-        self.wait(1.0)
-        for i in range(4, -1, -1):
-            self.play(Write(final_res[i]), run_time=0.8)
-            self.wait(1.5)
+        self.wait(2.5)
+        
+        # Birler
+        self.play(Write(final_res[4]), run_time=0.8)
+        self.wait(2.0)
+        
+        # Onlar
+        self.play(Write(final_res[3]), run_time=0.8)
+        self.play(Write(c1_add), run_time=0.8)
+        self.wait(2.5)
+        
+        # Yüzler
+        self.play(Write(final_res[2]), run_time=0.8)
+        self.play(FadeOut(c1_add))
+        self.wait(2.5)
+        
+        # Binler
+        self.play(Write(final_res[1]), run_time=0.8)
+        self.wait(2.0)
+        
+        # On Binler
+        self.play(Write(final_res[0]), run_time=0.8)
+        self.wait(3.5)
         
         self.wait(8.0)
